42ae59ae1f4c22d0d1f8483e7cd7dd3819d023b5
Mario Buntić <mario.buntic@adopto.eu>
2019-07-02 13:20:18 +0200

    test

--- gpt_2_simple/src/model.py
+++ gpt_2_simple/src/model.py
@@ -167,12 +167,13 @@ def model(hparams, X, past=None, scope='model', reuse=False,emb=False):
             presents.append(present)
         results['present'] = tf.stack(presents, axis=1)
         h = norm(h, 'ln_f')
-        if(emb):
-            return h
+
 
         # Language model loss.  Do tokens <n predict token n?
         h_flat = tf.reshape(h, [batch*sequence, hparams.n_embd])
         logits = tf.matmul(h_flat, wte, transpose_b=True)
         logits = tf.reshape(logits, [batch, sequence, hparams.n_vocab])
         results['logits'] = logits
+        if(emb):
+            results["h"]=h
         return results
